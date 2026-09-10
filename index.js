3ce3c5080e65d507386fb099e82beda8c9309a3c
sania mujtaba <136702463+sania28@users.noreply.github.com>
2026-09-10 16:44:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const route = require("./routes/routes")
-const connection = require("./database/db")
+const route = require("./routes/routes");
+const connection = require("./database/db");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
-app.use(cors());
 
+app.use(cors({
+    origin: "https://blog-platform-frontend-kftg.onrender.com",
+    credentials: true
+}));
 
+const PORT = process.env.PORT || 8080;
 
-const PORT = process.env.PORT || 8080
-app.listen(PORT, () => console.log("server started on", PORT))
+app.listen(PORT, () => console.log("server started on", PORT));
 
 app.use(fileUpload({
     useTempFiles: true,
-}))
+}));
 
 app.use(
     express.urlencoded({ extended: true })
 );
+
 app.use(express.json());
 
-app.use("/", route)
+app.use("/", route);
 
 // connecting to the dataBase
 connection();
