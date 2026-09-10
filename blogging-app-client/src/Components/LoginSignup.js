96bdc448cf442895d1100c32256b85d6ff685be3
sania mujtaba <136702463+sania28@users.noreply.github.com>
2026-09-10 16:11:28 +0530

    Update LoginSignup.js

--- blogging-app-client/src/Components/LoginSignup.js
+++ blogging-app-client/src/Components/LoginSignup.js
@@ -101,7 +101,7 @@ function LoginSignup() {
                             "content-type": "application/json"
                         }
                     }
-                    const response = await axios.post("http://localhost:8080/signup",
+                    const response = await axios.post("https://blog-platform-backend-zt3t.onrender.com/signup",
                         signUp, config
                     )
                     setAccount(response.data.username)
@@ -214,7 +214,7 @@ function LoginSignup() {
                         "content-type": "application/json"
                     }
                 }
-                const response = await axios.post("http://localhost:8080/login", logInValues, config);
+                const response = await axios.post("https://blog-platform-backend-zt3t.onrender.com/login", logInValues, config);
 
                 localStorage.setItem('accessToken', `Bearer ${response.data.accessToken}`)
                 localStorage.setItem('username', response.data.username)
