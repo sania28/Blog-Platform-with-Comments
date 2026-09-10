0f40054ba6fd952e09705e21b9a480c83a4cdad5
sania mujtaba <136702463+sania28@users.noreply.github.com>
2026-09-10 20:38:39 +0530

    Update authenticateToken.js

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -1,40 +1,27 @@
-
 require('dotenv').config();
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
 const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
 
-
-
-    const authHeader = req.headers['authorization'];
-
-
-    const token = authHeader && authHeader.split(' ')[1];
-
-
-    if (token === null) {
-        res.status(501).json({ msg: "cannot procced without a token!" })
+    if (!token) {
+        return res.status(401).json({
+            msg: "cannot procced without a token!"
+        });
     }
 
     jwt.verify(token, process.env.ACCESS_SECRET_KEY, (err, user) => {
-        
         if (err) {
-            res.status(502).json({ msg: "Invalid token found" });
-        }
-        else {
-            req.user = user;
-            console.log("user verified")
-            next();
-
+            return res.status(401).json({
+                msg: "Invalid token found"
+            });
         }
 
-    })
+        req.user = user;
+        console.log("user verified");
+        next();
+    });
+};
 
-
-
-
-
-
-}
-
-module.exports = authenticateToken
+module.exports = authenticateToken;
