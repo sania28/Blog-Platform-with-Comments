c75d3a163cc2588251f93e103e8246f19caf56b5
sania mujtaba <136702463+sania28@users.noreply.github.com>
2026-09-10 16:20:06 +0530

    Update MyPostCard.js

--- blogging-app-client/src/Components/home/MyPostCard.js
+++ blogging-app-client/src/Components/home/MyPostCard.js
@@ -37,7 +37,7 @@ export default function MyPostCard({ post }) {
       id: post._id,
     }
     try {
-      const response = axios.post("http://localhost:8080/delete", data, config)
+      const response = axios.post("https://blog-platform-backend-zt3t.onrender.com/delete", data, config)
       console.log(response.data.msg);
       window.location.reload(false)
 
